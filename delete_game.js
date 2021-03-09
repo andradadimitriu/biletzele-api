a948b2028993190838dc58840e952ace4bf1c6ca
andradadimitriu <andrada.dimitriu@gmail.com>
2021-03-09 21:39:50 +0200

    delete game

---
+++ delete_game.js
@@ -0,0 +1,16 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+import {IDENTIFIERS} from "./libs/identifiers";
+
+export const main = handler(async (event) => {
+    const params = {
+      TableName: process.env.tableName,
+      Key: {
+          PK: `GAME#${IDENTIFIERS.GAME_TYPE_BILETZELE}#${event.pathParameters.id}`,
+          SK: `#METADATA#${IDENTIFIERS.GAME_TYPE_BILETZELE}#${event.pathParameters.id}`
+      }
+  };
+    await dynamoDb.delete(params);
+    return { status: true };
+    }
+);
