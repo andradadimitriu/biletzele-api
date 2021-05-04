e6bebf2efccd0d6bf943aad9780e8eb60468c4e3
andradadimitriu <andrada.dimitriu@gmail.com>
2021-05-04 20:51:08 +0300

    fix - team name to accept any character

--- next_word_to_guess.js
+++ next_word_to_guess.js
@@ -6,9 +6,10 @@ export const main = handler(async (event) => {
     const data = JSON.parse(event.body);
     console.log(`data: ${JSON.stringify(data)}`);
     const params = {
-            TableName: process.env.tableName,
-            Key: { PK:`GAME#${IDENTIFIERS.GAME_TYPE_BILETZELE}#${event.pathParameters.id}`, SK: `#METADATA#${IDENTIFIERS.GAME_TYPE_BILETZELE}#${event.pathParameters.id}`},
-            UpdateExpression: `REMOVE rounds[${data.roundNo - 1}].wordsLeft[${data.oldWordIndex}] SET rounds[${data.roundNo - 1}].score.${data.teamTurn} =  rounds[${data.roundNo - 1}].score.${data.teamTurn} + :increment, turn.wordIndex =:newWordIndex`,
+        TableName: process.env.tableName,
+        Key: { PK:`GAME#${IDENTIFIERS.GAME_TYPE_BILETZELE}#${event.pathParameters.id}`, SK: `#METADATA#${IDENTIFIERS.GAME_TYPE_BILETZELE}#${event.pathParameters.id}`},
+        ExpressionAttributeNames:  {"#teamTurn": data.teamTurn},
+        UpdateExpression: `REMOVE rounds[${data.roundNo - 1}].wordsLeft[${data.oldWordIndex}] SET rounds[${data.roundNo - 1}].score.#teamTurn =  rounds[${data.roundNo - 1}].score.#teamTurn + :increment, turn.wordIndex =:newWordIndex`,
             ExpressionAttributeValues: {
                 ':turnNo': data.turnNo,
                 ':newWordIndex': data.newWordIndex,
